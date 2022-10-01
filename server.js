4ef6efc440bf4af75600f7e3f33ff3aaa794af9a
Prince Verma <princeverma360p@gmail.com>
2022-10-01 19:27:35 +0530

    preparation for deployment -2

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.URIREMOTE, {
 
 
 //server listening------------------------------------------------->
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`Server started on port ${port} 👍`);
 })
