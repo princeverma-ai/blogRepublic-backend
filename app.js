329d4f3b81d5220fa2972c988640d5cf951bca0e
Prince Verma <princeverma360p@gmail.com>
2022-10-03 23:06:31 +0530

    added packages

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
+
 const homeRouter = require('./routes/homeRouter');
 const blogRouter = require('./routes/blogRouter');
 const userRouter = require('./routes/userRouter');
@@ -14,6 +19,10 @@ const app = express();
 //middleware
 app.use(compression())
 app.use(express.json());
+app.use(mongoSanitize())
+app.use(xss());
+app.use(hpp());
+
 app.use(cors());
 app.use(express.static('public'));
 
