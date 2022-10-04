1316c9e28f4232eed056d6b027bdc40fdb2978f6
Prince Verma <princeverma360p@gmail.com>
2022-10-04 13:17:11 +0530

    fixes in blog model

--- models/blogModel.js
+++ models/blogModel.js
@@ -5,27 +5,21 @@ const mongoose = require('mongoose');
 const Schema = new mongoose.Schema({
     blogTitle: {
         type: String,
-        required: [true, "A Blog Title is required"]
     },
     blogDescription: {
         type: String,
-        required: [true, "A Blog Description is required"]
     },
     blogText: {
         type: String,
-        required: [true, "The Blog Text is required"]
     },
     blogCoverImage: {
         type: String,
-        required: [true, "A Blog Cover Image is required"]
     },
     blogPostTime: {
         type: Date,
-        required: [true, "Please specify the blog post time"]
     },
     blogPostedBy: {
         type: String,
-        required: [true, "Please specify who is adding this blog"]
     }
 
 });
